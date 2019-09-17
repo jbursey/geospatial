5f0c54f90ea668080548ddd2d839e77776304617
Jeffrey Bursey <jeffrey.bursey8806@live.com>
2019-09-17 12:34:51 -0500

    added some more geospatial constants. v1

--- Geospatial/Geospatial.Core/Constants.cs
+++ Geospatial/Geospatial.Core/Constants.cs
@@ -10,5 +10,13 @@ namespace Geospatial.Core
         public const double MIN_LNG = -180.0;
         public const double MAX_LAT = 90.0;
         public const double MAX_LNG = 180.0;
+
+        public const double EARTH_RADIUS_EQUATOR_METERS = 6378137.0;
+        public const double EARTH_RADIUS_POLAR_METERS = 6356752.3;
+
+        public const double MERCATOR_MAX_LNG = 180.0;
+        public const double MERCATOR_MIN_LNG = -180.0;
+        public const double MERCATOR_MAX_LAT = 85.051129;
+        public const double MERCATOR_MIN_LAT = -85.051129;
     }
 }
