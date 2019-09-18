97aab48144b47d023a4467518297ab4b42f105d0
Jeffrey Bursey <jeffrey.bursey8806@live.com>
2019-09-18 13:41:14 -0500

    Adding logic as I have time.

--- Geospatial/Geospatial.Core/MBR.cs
+++ Geospatial/Geospatial.Core/MBR.cs
@@ -13,6 +13,24 @@ namespace Geospatial.Core
 
         public Point Northeast { get; set; }
         public Point Southwest { get; set; }
+
+        public Polygon ToPolygon()
+        {
+            Polygon p = new Polygon();
+
+            List<Point> points = new List<Point>();
+
+            Point nw = new Point(Southwest.X, Northeast.Y);
+            Point se = new Point(Northeast.X, Southwest.Y);
+
+            points.Add(nw);
+            points.Add(Northeast);
+            points.Add(se);
+            points.Add(Southwest);
+            points.Add(nw);
+
+            return p;
+        }
     }
 
 
