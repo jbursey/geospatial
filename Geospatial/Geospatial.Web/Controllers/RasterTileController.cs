6243cb1fe705299688c142eecaf66e2aa5c33857
Jeffrey Bursey <jeffrey.bursey8806@live.com>
2019-10-07 15:13:02 -0500

    prepping demo client to showcase tile stuff.

---
+++ Geospatial/Geospatial.Web/Controllers/RasterTileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Geospatial.Tiles.Raster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geospatial.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RasterTileController : ControllerBase
+    {
+        private RasterTileRenderer _renderer;
+
+        public RasterTileController(RasterTileRenderer renderer)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("sample/{name}/{x}/{y}/{z}")]
+        public FileContentResult GetSampleTileData(string name, int x, int y, int z)
+        {
+            byte[] data = null;
+
+            FileContentResult contentResult = new FileContentResult(data, "application/octet-stream");
+
+            return contentResult;
+        }
+    }
+}
--- Geospatial/Geospatial.Web/Startup.cs
+++ Geospatial/Geospatial.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Geospatial.Tiles.Raster;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -30,6 +31,7 @@ namespace Geospatial.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddSingleton<RasterTileRenderer>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
